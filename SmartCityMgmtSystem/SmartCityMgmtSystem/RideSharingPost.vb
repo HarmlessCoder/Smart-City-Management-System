dfc3997c984c4d60498084caad95a63aa5a72ddc
dhaneshragu <dhanesh.v@iitg.ac.in>
2024-03-12 23:27:17 +0530

    ride sharing basic UI

--- SmartCityMgmtSystem/SmartCityMgmtSystem/RideSharingPost.vb
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/RideSharingPost.vb
@@ -1,3 +1,23 @@
 ﻿Public Class RideSharingPost
+    Public Sub SetDetails(name As String,
+                           Optional datetime As String = "",
+                           Optional fromPlace As String = "",
+                           Optional toPlace As String = "",
+                           Optional fare As Integer = 0,
+                           Optional capacity As Integer = 0,
+                          Optional image As Image = Nothing)
+        ' Set the labels
+        lblname.Text = "       " & name
+        lbldt.Text = "       " & datetime
+        lblfrom.Text = "      " & fromPlace
+        lblto.Text = "      " & toPlace
+        lblfare.Text = "       ₹" & fare
+        lblcapacity.Text = "       " & capacity
+
+        ' Set the picture box
+        If image IsNot Nothing Then
+            picbox.Image = image
+        End If
+    End Sub
 
 End Class
