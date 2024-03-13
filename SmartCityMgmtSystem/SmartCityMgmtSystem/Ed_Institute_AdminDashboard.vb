057803bcccfd67236ca1bdafaaf614f1bafd8e80
soumyadeep-p <paulsoumyadeep2019@gmail.com>
2024-03-13 23:12:34 +0530

    Updated ed dashboards

--- SmartCityMgmtSystem/SmartCityMgmtSystem/Ed_Institute_AdminDashboard.vb
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/Ed_Institute_AdminDashboard.vb
@@ -1,4 +1,4 @@
-﻿Public Class Ed_Stud_Moodle
+﻿Public Class Ed_Institute_AdminDashboard
     Private Sub childformPanel_Paint(sender As Object, e As PaintEventArgs) Handles childformPanel.Paint
 
     End Sub
