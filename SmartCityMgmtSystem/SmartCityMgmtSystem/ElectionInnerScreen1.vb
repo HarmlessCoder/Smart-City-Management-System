30a82d96a21b4674cd2247b18a1f25abf8090413
sirik <sanjanasiri3004@gmail.com>
2024-03-15 04:38:24 +0530

    Added registration as voter

--- SmartCityMgmtSystem/SmartCityMgmtSystem/ElectionInnerScreen1.vb
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/ElectionInnerScreen1.vb
@@ -4,7 +4,7 @@ Public Class ElectionInnerScreen1
 
     End Sub
 
-    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+    Private Sub Panel1_Click(sender As Object, e As EventArgs) Handles Panel1.Click
 
     End Sub
 
---
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/ElectionInnerScreenVoter.vb
@@ -0,0 +1,90 @@
+﻿Imports System.Data.SqlClient
+Public Class ElectionInnerScreenVoter
+    Private Sub ElectionInnerScreen1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub Panel3_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub Panel2_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub Panel5_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Panel6_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub Panel4_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Panel2_Paint_1(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox4_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Panel4_Paint_1(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub PictureBox5_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox6_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Panel5_Paint_1(sender As Object, e As PaintEventArgs) Handles Panel5.Paint
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+End Class
