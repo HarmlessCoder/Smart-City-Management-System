4bfb48360050bc5dcab9270148f0feadc9b9c933
Varsha <varsha25032003@gmail.com>
2024-03-19 19:42:16 +0530

    Banking module added

---
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/BankingDashboard.vb
@@ -0,0 +1,32 @@
+﻿Public Class BankingDashboard
+
+    Private Sub TransportationDashboard_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs)
+        'View the TransportationAdminHome screen by default - first argument, name of the panel in the parent panel, second - name of the child form
+        Globals.viewChildForm(childformPanel, TransportationAdminHome)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Globals.viewChildForm(childformPanel, UserBankDetails)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Globals.viewChildForm(childformPanel, CheckBankBalance)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Globals.viewChildForm(childformPanel, ViewBankTransactions)
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Globals.viewChildForm(childformPanel, PayBills)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Globals.viewChildForm(childformPanel, RechargeFastTag)
+    End Sub
+
+End Class
