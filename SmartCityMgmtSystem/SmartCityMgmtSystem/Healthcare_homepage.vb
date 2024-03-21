a84ac93e5bf461933e7d37fa1be3602608d7a144
Dhanesh <95169037+dhaneshragu@users.noreply.github.com>
2024-03-21 12:22:17 +0530

    Revert "Revert "Added healthcare module pages""

---
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/Healthcare_homepage.vb
@@ -0,0 +1,34 @@
+﻿Public Class Healthcare_homepage
+    Private Sub book_appointment_Click(sender As Object, e As EventArgs) Handles book_appointment.Click
+        ' Navigate to hc_B_Appointment page
+        Dim appointmentPage As New hc_B_Appointment()
+        appointmentPage.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub pharmacy_Click(sender As Object, e As EventArgs) Handles pharmacy.Click
+        ' Navigate to hc_Pharmacy page
+        Dim pharmacyPage As New hc_Pharmacy()
+        pharmacyPage.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub donate_blood_Click(sender As Object, e As EventArgs) Handles donate_blood.Click
+        ' Navigate to hc_Donate_Blood page
+        Dim donateBloodPage As New hc_Donate_Blood()
+        donateBloodPage.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub history_Click(sender As Object, e As EventArgs) Handles history.Click
+        ' Navigate to hc_History page
+        Dim historyPage As New hc_History()
+        historyPage.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub emergency_Click(sender As Object, e As EventArgs) Handles emergency.Click
+        ' Navigate to hc_Emergency page (if you have one)
+        ' Add similar logic for emergency page if needed
+    End Sub
+End Class
