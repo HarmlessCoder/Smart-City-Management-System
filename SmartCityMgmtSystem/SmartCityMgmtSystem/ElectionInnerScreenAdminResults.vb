bce696ea08cd8af22a5c8d7cc4be84c4129afa97
sirik <sanjanasiri3004@gmail.com>
2024-03-24 15:53:35 +0530

    Added Results in Admin

--- SmartCityMgmtSystem/SmartCityMgmtSystem/ElectionInnerScreenAdminResults.vb
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/ElectionInnerScreenAdminResults.vb
@@ -1,42 +1,51 @@
 ﻿Imports System.Data.SqlClient
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports MySql.Data.MySqlClient
 Public Class ElectionInnerScreenAdminResults
     Private Sub LoadandBindDataGridView()
-        'Get connection String from globals
-        Dim conString = Globals.getdbConnectionString()
-        Dim Con = New SqlConnection(conString)
+        'Get connection from globals
+        Dim Con = Globals.GetDBConnection()
+        Dim reader As MySqlDataReader
+        Dim cmd As MySqlCommand
 
-        'Query for SQL table
-        Dim query = "enter your query"
-        Con.Open()
+        Try
+            Con.Open()
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
 
-        Dim cmd As New SqlCommand(query, Con)
-        Dim adapter As New SqlDataAdapter(cmd)
-
+        cmd = New MySqlCommand("SELECT * FROM result_table", Con)
+        reader = cmd.ExecuteReader
         ' Create a DataTable to store the data
         Dim dataTable As New DataTable()
 
         'Fill the DataTable with data from the SQL table
-        adapter.Fill(dataTable)
+        dataTable.Load(reader)
+        reader.Close()
+        Con.Close()
 
         'IMP: Specify the Column Mappings from DataGridView to SQL Table
         DataGridView1.AutoGenerateColumns = False
-        DataGridView1.Columns(0).DataPropertyName = "Column Name in SQL table"
-        DataGridView1.Columns(1).DataPropertyName = "Column Name in SQL table"
+        DataGridView1.Columns(0).DataPropertyName = "ministry_id"
+        DataGridView1.Columns(1).DataPropertyName = "winner_id"
+        DataGridView1.Columns(2).DataPropertyName = "votes"
+        DataGridView1.Columns(3).DataPropertyName = "turnout_ministry"
 
         ' Bind the data to DataGridView
         DataGridView1.DataSource = dataTable
     End Sub
     Private Sub ElectionInnerScreen1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        For i As Integer = 1 To 8
-            ' Add an empty row to the DataGridView
-            Dim row As New DataGridViewRow()
-            DataGridView1.Rows.Add(row)
+        'For i As Integer = 1 To 8
+        ' Add an empty row to the DataGridView
+        'Dim' row As New DataGridViewRow()
+        'DataGridView1.Rows.Add(row)
 
-            ' Set values for the first three columns in the current row
-            DataGridView1.Rows(i - 1).Cells("Column1").Value = "DummyVal"
-            DataGridView1.Rows(i - 1).Cells("Column2").Value = "DummyVal"
-            DataGridView1.Rows(i - 1).Cells("Column3").Value = "DummyVal"
-        Next
+        ' Set values for the first three columns in the current row
+        'DataGridView1.Rows(i - 1).Cells("Column1").Value = "DummyVal"
+        'DataGridView1.Rows(i - 1).Cells("Column2").Value = "DummyVal"
+        'DataGridView1.Rows(i - 1).Cells("Column3").Value = "DummyVal"
+        'Next
+        LoadandBindDataGridView()
     End Sub
 
     Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs)
--- SmartCityMgmtSystem/SmartCityMgmtSystem/My Project/Application.Designer.vb
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.SmartCityMgmtSystem.TransportMangeBusStopAdmin
+            Me.MainForm = Global.SmartCityMgmtSystem.ElectionDashboard
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
