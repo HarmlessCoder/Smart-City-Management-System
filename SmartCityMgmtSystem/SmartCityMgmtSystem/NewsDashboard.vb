f06e0843d304b00546111649162943be2641de33
sirik <sanjanasiri3004@gmail.com>
2024-03-24 14:13:59 +0530

    Added NewsDasboard

---
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/NewsDashboard.vb
@@ -0,0 +1,11 @@
+﻿Public Class NewsDashboard
+
+    Private Sub TransportationDashboard_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        'View the TransportationAdminHome screen by default - first argument, name of the panel in the parent panel, second - name of the child form
+        Globals.viewChildForm(childformPanel, TransportationAdminHome)
+    End Sub
+End Class
