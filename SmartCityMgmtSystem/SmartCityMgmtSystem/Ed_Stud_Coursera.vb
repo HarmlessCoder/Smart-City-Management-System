8faa7e4b7901fb07c63b1ea8b03bbabc776e65e6
Soumyadeep Paul <p.soumyadeep@iitg.ac.in>
2024-03-25 15:27:49 +0530

    Added CourseResource page and Content page

--- SmartCityMgmtSystem/SmartCityMgmtSystem/Ed_Stud_Coursera.vb
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/Ed_Stud_Coursera.vb
@@ -10,4 +10,8 @@
     Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
         Ed_GlobalDashboard.OpenFormInGlobalEdPanel(Ed_StudentDashboard)
     End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Globals.viewChildForm(childformPanel, New Ed_Coursera_CourseContent(1, childformPanel))
+    End Sub
 End Class
