929000a09120c6a33c81339a8806bf54711dec78
unknown <vijeshrbhat@gmail.com>
2024-03-29 19:20:37 +0530

    employment_admin_add page

--- SmartCityMgmtSystem/SmartCityMgmtSystem/Employment_portal_admin_add.vb
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/Employment_portal_admin_add.vb
@@ -1,11 +1,12 @@
-﻿Public Class Employment_portal_admin
+﻿Public Class Employment_portal_admin_add
 
     Private Sub TransportationDashboard_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
 
     End Sub
 
-    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs)
         'View the TransportationAdminHome screen by default - first argument, name of the panel in the parent panel, second - name of the child form
-        Globals.viewChildForm(childformPanel, Employment_portal_admin_add)
+        Globals.viewChildForm(childformPanel, TransportationAdminHome)
     End Sub
+
 End Class
