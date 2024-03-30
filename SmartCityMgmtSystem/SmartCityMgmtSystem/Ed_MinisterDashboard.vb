64f8496556420fe173278e40bba1aacbc7db83d3
Mihit <mihitsreejith2@gmail.com>
2024-03-30 05:44:55 +0530

    added edit institute

--- SmartCityMgmtSystem/SmartCityMgmtSystem/Ed_MinisterDashboard.vb
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/Ed_MinisterDashboard.vb
@@ -4,6 +4,14 @@
     End Sub
 
     Private Sub Ed_MinisterDashboard_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Globals.viewChildForm(childformPanel, New Ed_Institute_List())
+        Dim ed_list As New Ed_Institute_List(childformPanel)
+        ed_list.user_type = "Admin"
+        Globals.viewChildForm(childformPanel, ed_list)
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim ed_list As New Ed_Institute_List(childformPanel)
+        ed_list.user_type = "Admin"
+        Globals.viewChildForm(childformPanel, ed_list)
     End Sub
 End Class
