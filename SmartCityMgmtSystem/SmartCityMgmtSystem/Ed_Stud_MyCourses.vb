f0ee3dc061f8d0d52cabe42dda1c0b3bc8968d0d
Soumyadeep Paul <p.soumyadeep@iitg.ac.in>
2024-03-31 14:06:52 +0530

    Coursera course Views done

--- SmartCityMgmtSystem/SmartCityMgmtSystem/Ed_Stud_MyCourses.vb
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/Ed_Stud_MyCourses.vb
@@ -1,47 +1,40 @@
 ﻿Imports System.Data.SqlClient
+Imports SmartCityMgmtSystem.Ed_Coursera_Handler
 Public Class Ed_Stud_MyCourses
+    Dim handler As New Ed_Coursera_Handler()
     Private Sub Ed_Stud_Coursera_Home_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Button1_Click(sender, e)
     End Sub
 
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub Label6_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs)
-
-    End Sub
-
-    Private Sub FlowLayoutPanel1_Paint(sender As Object, e As PaintEventArgs) Handles FlowLayoutPanel1.Paint
-
-    End Sub
-
-    Private Sub Label6_Click_1(sender As Object, e As EventArgs)
-
-    End Sub
-
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        FlowLayoutPanel1.Controls.Clear()
+
         Button1.BackColor = Color.FromArgb(88, 133, 175)
         Button2.BackColor = Color.FromArgb(40, 68, 114)
-        Dim labels As Ed_CourseProgress() = New Ed_CourseProgress(21) {}
 
-        ' Create labels and set properties
-        For i As Integer = 0 To 20
+        Dim courses As Course() = handler.GetInProgressCourses(Ed_GlobalDashboard.userID)
+
+        ' Create Ed_CourseProgress objects and set properties
+        Dim labels As Ed_CourseProgress() = New Ed_CourseProgress(courses.Length - 1) {}
+
+        For i As Integer = 0 To courses.Length - 1
             labels(i) = New Ed_CourseProgress()
-            labels(i).CourseID = i
-            labels(i).ProgressBar1.Value = i * 5
+            labels(i).CourseID = courses(i).CourseID
+            labels(i).CourseItem = courses(i)
+
         Next
 
-        ' Add labels to the FlowLayoutPanel
+        FlowLayoutPanel1.Controls.Clear()
+        ' Add Ed_CourseProgress objects to the FlowLayoutPanel
         For Each Ed_CourseProgress As Ed_CourseProgress In labels
             FlowLayoutPanel1.Controls.Add(Ed_CourseProgress)
         Next
+
+
+
+
+
+
 
     End Sub
 
