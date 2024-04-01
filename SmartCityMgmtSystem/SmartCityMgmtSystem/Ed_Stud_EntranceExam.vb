4feaae2c7b6fadc5a15914189066a497ec3f7a4c
Tanish <106595510+v-tanish012@users.noreply.github.com>
2024-04-02 03:30:31 +0530

    ed entrance exam fetch sql query logiv

--- SmartCityMgmtSystem/SmartCityMgmtSystem/Ed_Stud_EntranceExam.vb
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/Ed_Stud_EntranceExam.vb
@@ -8,50 +8,46 @@
     End Sub
 
     Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
-        Dim entranceScreen As New Ed_EntranceInnerScreen()
-        entranceScreen.labelText = "Engineering Entrance Exam"
-        Globals.viewChildForm(Ed_GlobalDashboard.innerpanel, entranceScreen)
+        OpenEntranceScreen("Engineering Entrance Exam", 1)
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim entranceScreen As New Ed_EntranceInnerScreen()
-        entranceScreen.labelText = "Medical Entrance Exam"
-        Globals.viewChildForm(Ed_GlobalDashboard.innerpanel, entranceScreen)
+        OpenEntranceScreen("Medical Entrance Exam", 2)
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Dim entranceScreen As New Ed_EntranceInnerScreen()
-        entranceScreen.labelText = "Arts Entrance Exam"
-        Globals.viewChildForm(Ed_GlobalDashboard.innerpanel, entranceScreen)
+        OpenEntranceScreen("Arts Entrance Exam", 3)
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        Dim entranceScreen As New Ed_EntranceInnerScreen()
-        entranceScreen.labelText = "Commerce Entrance Exam"
-        Globals.viewChildForm(Ed_GlobalDashboard.innerpanel, entranceScreen)
+        OpenEntranceScreen("Commerce Entrance Exam", 4)
     End Sub
 
     Private Sub PictureButtonvb1_Click(sender As Object, e As EventArgs) Handles PictureButtonvb1.Click
-        Dim entranceScreen As New Ed_EntranceInnerScreen()
-        entranceScreen.labelText = "Engineering Entrance Exam"
-        Globals.viewChildForm(Ed_GlobalDashboard.innerpanel, entranceScreen)
+        OpenEntranceScreen("Engineering Entrance Exam", 1)
     End Sub
 
     Private Sub PictureButtonvb4_Click(sender As Object, e As EventArgs) Handles PictureButtonvb4.Click
-        Dim entranceScreen As New Ed_EntranceInnerScreen()
-        entranceScreen.labelText = "Medical Entrance Exam"
-        Globals.viewChildForm(Ed_GlobalDashboard.innerpanel, entranceScreen)
+        OpenEntranceScreen("Medical Entrance Exam", 2)
     End Sub
 
     Private Sub PictureButtonvb3_Click(sender As Object, e As EventArgs) Handles PictureButtonvb3.Click
-        Dim entranceScreen As New Ed_EntranceInnerScreen()
-        entranceScreen.labelText = "Arts Entrance Exam"
-        Globals.viewChildForm(Ed_GlobalDashboard.innerpanel, entranceScreen)
+        OpenEntranceScreen("Arts Entrance Exam", 3)
     End Sub
 
     Private Sub PictureButtonvb2_Click(sender As Object, e As EventArgs) Handles PictureButtonvb2.Click
+        OpenEntranceScreen("Commerce Entrance Exam", 4)
+    End Sub
+
+    Private Sub OpenEntranceScreen(labelText As String, examID As Integer)
         Dim entranceScreen As New Ed_EntranceInnerScreen()
-        entranceScreen.labelText = "Commerce Entrance Exam"
+        entranceScreen.labelText = labelText
+        entranceScreen.examID = examID
         Globals.viewChildForm(Ed_GlobalDashboard.innerpanel, entranceScreen)
+    End Sub
+
+
+    Private Sub Ed_Stud_EntranceExam_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
     End Sub
 End Class
