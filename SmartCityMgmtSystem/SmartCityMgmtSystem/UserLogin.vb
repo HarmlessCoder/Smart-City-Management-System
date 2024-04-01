30097b8d0922d6a7c80a8891e9af1a5f2383fc96
lakshya <lakshya30onkar@gmail.com>
2024-04-02 01:25:25 +0530

    center screen

--- SmartCityMgmtSystem/SmartCityMgmtSystem/UserLogin.vb
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/UserLogin.vb
@@ -1,6 +1,7 @@
 ﻿Imports MySql.Data.MySqlClient
 Imports Mysqlx.XDevAPI.Common
 Imports System.Security.Cryptography
+Imports System.Web
 
 Public Class UserLogin
 
@@ -44,7 +45,7 @@ Public Class UserLogin
                     TextBox2.Clear()
                     TextBox3.Clear()
                 ElseIf TextBox2.Text <> mail Then
-                    MessageBox.Show("Incorrect EmailID or UserID.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    MessageBox.Show("Incorrect Email ID or UserID.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
                     TextBox1.Clear()
                     TextBox2.Clear()
                     TextBox3.Clear()
@@ -63,7 +64,7 @@ Public Class UserLogin
                 TextBox3.Clear()
             End Try
         Else
-            MessageBox.Show("Please enter User ID or Email ID.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            MessageBox.Show("Please enter User ID and Email ID.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End If
 
 
@@ -72,12 +73,42 @@ Public Class UserLogin
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim pass As String = ""
-        If TextBox2.Text IsNot Nothing AndAlso Not IsDBNull(TextBox2.Text) Then
-            Dim otp = New UserGetOtp(TextBox2.Text, pass, 1)
-            otp.Show()
-            Me.Close()
-        Else
-            MessageBox.Show("Please enter your Email ID.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Dim mail As String = ""
+        Dim uid As Integer = -1
+        If Not String.IsNullOrWhiteSpace(TextBox1.Text) Then
+            Try
+                uid = Convert.ToInt32(TextBox1.Text)
+                Dim conStr As String = Globals.getdbConnectionString()
+                Dim cmd As String = "SELECT email FROM users WHERE user_id = @uid"
+                Using connection As New MySqlConnection(conStr)
+                    Using sqlCommand As New MySqlCommand(cmd, connection)
+                        sqlCommand.Parameters.AddWithValue("@uid", TextBox1.Text)
+                        ' Execute the query and retrieve the user ID
+                        connection.Open()
+                        Using reader As MySqlDataReader = sqlCommand.ExecuteReader()
+                            If reader.Read() Then
+                                mail = Convert.ToString(reader("email"))
+                            End If
+                        End Using
+                    End Using
+                End Using
+
+                If TextBox2.Text <> mail Then
+                    MessageBox.Show("Incorrect Email ID or UserID.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    TextBox1.Clear()
+                    TextBox2.Clear()
+                    TextBox3.Clear()
+                Else
+                    Dim otp = New UserGetOtp(TextBox2.Text, pass, 1)
+                    otp.Show()
+                    Me.Close()
+                End If
+            Catch ex As Exception
+                MessageBox.Show("Error converting TextBox1.Text to integer: " & ex.Message)
+                TextBox1.Clear()
+                TextBox2.Clear()
+                TextBox3.Clear()
+            End Try
         End If
 
     End Sub
