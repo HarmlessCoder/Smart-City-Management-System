5761b9ac607d4c0a5b15ccbe012cdf15bd50ec75
Soumyadeep Paul <p.soumyadeep@iitg.ac.in>
2024-04-02 15:09:50 +0530

    Moodle course navigation done

--- SmartCityMgmtSystem/SmartCityMgmtSystem/Ed_Moodle_CourseContent.vb
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/Ed_Moodle_CourseContent.vb
@@ -1,35 +1,60 @@
 ﻿Imports System.Data.SqlClient
+Imports SmartCityMgmtSystem.Ed_Moodle_Handler
 Public Class Ed_Moodle_CourseContent
-    Private RoomID As Integer
     Private callingPanel As Panel
+    Dim handler As New Ed_Moodle_Handler()
+    Public CourseContent As Ed_Moodle_Handler.MoodleCourse
 
 
     ' Constructor that accepts a Panel parameter
-    Public Sub New(roomID As Integer, panel As Panel)
+    Public Sub New(panel As Panel)
         InitializeComponent()
-        roomID = roomID
         callingPanel = panel
     End Sub
 
-    Private Sub Ed_Moodle_CourseContent_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim labels As Ed_ResourceLinkItem() = New Ed_ResourceLinkItem(8) {}
+    Private Sub LoadCourseDetails(ByVal roomID As Integer)
+        ' Call the function to load course details
+        Dim CourseDetails As MoodleCourse = handler.LoadCourseDetails(roomID)
+        CourseContent = CourseDetails
+        ' Check if course details are retrieved
+        If courseDetails IsNot Nothing Then
+            ' Set the labels with the retrieved data
+            Label1.Text = CourseDetails.Name
+            Label2.Text = "Instructor: " & CourseDetails.ProfName
+            Label3.Text = "Institute: " & CourseDetails.InstName
+        Else
+            MessageBox.Show("No course details found for the specified Room_ID.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
 
-        ' Create labels and set properties
-        For i As Integer = 0 To 7
-            labels(i) = New Ed_ResourceLinkItem()
-            labels(i).Label1.Text = "Resource " & (i + 1)
-            AddHandler labels(i).Label1.Click, AddressOf Label_Click ' Add click event handler
-        Next
-        ' Add labels to the FlowLayoutPanel
-        For Each Label As Ed_ResourceLinkItem In labels
-            FlowLayoutPanel1.Controls.Add(Label)
-        Next
+
+
+
+    Private Sub Ed_Moodle_CourseContent_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim RoomID As Integer = CourseContent.RoomID
+        LoadCourseDetails(RoomID)
+
+        ' Declare contents outside of the If block
+        Dim contents As RoomContent()
+        contents = handler.GetRoomContents(RoomID)
+
+        ' Check if there are any contents
+        If contents IsNot Nothing AndAlso contents.Length > 0 Then
+            ' Create labels and set properties based on content details
+            For Each content As RoomContent In contents
+                Dim contentItem As New Ed_MoodleResouceLinkItem()
+                contentItem.callingPanel = callingPanel
+                contentItem.Label1.Text = content.ContentName ' Assuming Label1 is used to display content names
+                ' Set other properties of contentItem based on content details if needed
+                contentItem.content = content
+                ' Add contentItem to the FlowLayoutPanel
+                FlowLayoutPanel1.Controls.Add(contentItem)
+            Next
+        Else
+            MessageBox.Show("No course content found for the specified Room_ID.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
     End Sub
-    Private Sub Label_Click(sender As Object, e As EventArgs)
-        Dim resourceForm As New Ed_Moodle_CourseResource(callingPanel, "Moodle")
-        resourceForm.Name = "HELLO"
-        Globals.viewChildForm(callingPanel, resourceForm)
-    End Sub
+
 
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
