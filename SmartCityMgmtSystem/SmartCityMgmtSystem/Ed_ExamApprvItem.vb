9aacb446c0ca646e81891bb09a99c9e53f7b56fb
Tanish <106595510+v-tanish012@users.noreply.github.com>
2024-04-11 02:17:06 +0530

    entrance exam almost done

--- SmartCityMgmtSystem/SmartCityMgmtSystem/Ed_ExamApprvItem.vb
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/Ed_ExamApprvItem.vb
@@ -1,3 +1,21 @@
 ﻿Public Class Ed_ExamApprvItem
+    Public examID As Integer
+    Public studentID As Integer
+    Private entranceExamHandler As New Ed_EntranceExam_Handler()
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If (Button2.Text = "Approve") Then
+            entranceExamHandler.approve_pending_request(examID, studentID, DateTime.Now.Year)
+            Button1.Hide()
+            Button2.Text = "Approved"
+        End If
 
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (Button1.Text = "Reject") Then
+            entranceExamHandler.reject_pending_request(examID, studentID, DateTime.Now.Year)
+            Button2.Hide()
+            Button1.Text = "Rejected"
+        End If
+    End Sub
 End Class
--- SmartCityMgmtSystem/SmartCityMgmtSystem/Ed_ManageEntrExam.vb
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/Ed_ManageEntrExam.vb
@@ -1,5 +1,24 @@
 ﻿Imports System.Data.SqlClient
+Imports MySql.Data.MySqlClient
 Public Class Ed_ManageEntrExam
+    Private examID As Integer
+    Private entranceExamHandler As New Ed_EntranceExam_Handler()
+    Private details As Ed_EntranceExam_Handler.EEDetails
+    Private venues As List(Of Ed_EntranceExam_Handler.EEVenue)
+    Private pendingAdmits As List(Of Ed_EntranceExam_Handler.EEStudentAdmit)
+    Private About As String
+    Private Syllabus As String
+    Private updatedSyllabus As String
+    Private updatedAbout As String
+    Private newVenueName As String
+    Private newVenueAddress As String
+    Private newVenueCapacity As Integer
+
+    ' Parameterized constructor to set the value for examID
+    Public Sub New(examID As Integer)
+        InitializeComponent()
+        Me.examID = examID
+    End Sub
 
     Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
         Globals.viewChildForm(Ed_GlobalDashboard.innerpanel, New Ed_ManageEntrHome())
@@ -7,42 +26,106 @@ Public Class Ed_ManageEntrExam
     End Sub
 
     Private Sub Ed_ManageEntrExam_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim labels As Ed_ExamApprvItem() = New Ed_ExamApprvItem(21) {}
+        details = entranceExamHandler.get_ee_details(examID)
+        venues = entranceExamHandler.get_all_venues()
+        pendingAdmits = entranceExamHandler.extract_pending_student_admits(examID)
+        Label11.Text = details.Name
+        ComboBox1.Text = details.Exam_application
+        ComboBox2.Text = details.Exam_results
+        RichTextBox1.Text = details.Notice_Message
+        RichTextBox2.Text = details.Contact_email
+        RichTextBox3.Text = details.Contact_Info
+        About = details.About
+        Syllabus = details.Syllabus
+        updatedSyllabus = Syllabus
+        updatedAbout = About
+        DateTimePicker1.Value = details.Date_Time
 
-        ' Create labels and set properties
-        For i As Integer = 0 To 10
-            labels(i) = New Ed_ExamApprvItem()
+
+        For Each pending As Ed_EntranceExam_Handler.EEStudentAdmit In pendingAdmits
+            Dim pendingItem As New Ed_ExamApprvItem()
+            pendingItem.examID = examID
+            pendingItem.studentID = pending.Student_ID
+            pendingItem.Label2.Text = "Name : " + pending.User_Name
+            pendingItem.Label1.Text = "Contact : " + pending.Phone_Number
+            pendingItem.Label3.Text = "UID : " + pending.Student_ID.ToString()
+            ' Set properties of venueItem using venue data
+            FlowLayoutPanel1.Controls.Add(pendingItem)
         Next
 
-        ' Add labels to the FlowLayoutPanel
-        For Each Ed_ExamApprvItem As Ed_ExamApprvItem In labels
-            FlowLayoutPanel1.Controls.Add(Ed_ExamApprvItem)
-        Next
-        Dim labels1 As Ed_VenueItem() = New Ed_VenueItem(21) {}
-
-        ' Create labels and set properties
-        For i As Integer = 0 To 10
-            labels1(i) = New Ed_VenueItem()
-        Next
-
-        ' Add labels to the FlowLayoutPanel
-        For Each Ed_VenueItem As Ed_VenueItem In labels1
-            FlowLayoutPanel2.Controls.Add(Ed_VenueItem)
+        For Each venue As Ed_EntranceExam_Handler.EEVenue In venues
+            Dim venueItem As New Ed_VenueItem()
+            venueItem.Label1.Text = venue.Capacity.ToString()
+            venueItem.Label2.Text = venue.Name
+            venueItem.Label3.Text = venue.Address
+            ' Set properties of venueItem using venue data
+            FlowLayoutPanel2.Controls.Add(venueItem)
         Next
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim enterSumForm As New Ed_Teacher_AddSummary()
-        enterSumForm.ShowDialog() ' Show as dialog if needed
+        Dim updateForm As New Ed_UpdateForm()
+        updateForm.original = updatedAbout
+        updateForm.ShowDialog()
+
+        ' After the update form is closed, update the values if the DialogResult is OK
+        If updateForm.DialogResult = DialogResult.OK Then
+            updatedAbout = updateForm.updated
+        End If
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        Dim enterSumForm As New Ed_Teacher_AddSummary()
-        enterSumForm.ShowDialog() ' Show as dialog if needed
+
+        Dim updateForm As New Ed_UpdateForm()
+        updateForm.original = updatedSyllabus
+        updateForm.ShowDialog()
+
+        ' After the update form is closed, update the values if the DialogResult is OK
+        If updateForm.DialogResult = DialogResult.OK Then
+            updatedSyllabus = updateForm.updated
+        End If
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Dim addnewVenue As New Ed_newVenue()
         addnewVenue.ShowDialog()
+
+        If addnewVenue.DialogResult = DialogResult.OK Then
+            newVenueAddress = addnewVenue.venueAddress
+            newVenueCapacity = addnewVenue.venueCapacity
+            newVenueName = addnewVenue.venueName
+            entranceExamHandler.add_new_venue(newVenueName, newVenueAddress, newVenueCapacity)
+            FlowLayoutPanel2.Controls.Clear()
+            venues = entranceExamHandler.get_all_venues()
+            For Each venue As Ed_EntranceExam_Handler.EEVenue In venues
+                Dim venueItem As New Ed_VenueItem()
+                venueItem.Label1.Text = venue.Capacity.ToString()
+                venueItem.Label2.Text = venue.Name
+                venueItem.Label3.Text = venue.Address
+                ' Set properties of venueItem using venue data
+                FlowLayoutPanel2.Controls.Add(venueItem)
+            Next
+        End If
     End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim _details As Ed_EntranceExam_Handler.EEDetails
+        _details.About = updatedAbout
+        _details.Syllabus = updatedSyllabus
+        _details.Name = details.Name
+        If (CheckBox1.Checked) Then
+            _details.Date_Time = DateTimePicker1.Value
+        Else
+            _details.Date_Time = details.Date_Time
+        End If
+        _details.Venue_List = details.Venue_List
+        _details.Exam_application = ComboBox1.Text
+        _details.Exam_results = ComboBox2.Text
+        _details.Notice_Message = RichTextBox1.Text
+        _details.Contact_email = RichTextBox2.Text
+        _details.Contact_Info = RichTextBox3.Text
+        entranceExamHandler.update_ee_details(examID, _details)
+    End Sub
+
+
 End Class
