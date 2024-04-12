347cf6f0bbb3ec79540e4092808343a57d0dd6a1
Gautam Juneja <g.juneja@iitg.ac.in>
2024-04-13 01:03:30 +0530

    return book done

--- SmartCityMgmtSystem/SmartCityMgmtSystem/lib_adminMT.vb
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/lib_adminMT.vb
@@ -305,142 +305,157 @@ Public Class lib_adminMT
             '        End Try
             '    End Using
             'End Using
-            If isStudent = False And isFaculty = False Then
+            If isStudent = False Then
                 MessageBox.Show("User with given id does not exist.")
                 Return
             End If
 
             'Check whether the book is issued to the user
-            userQuery = "SELECT * FROM borrowed_books WHERE (BookID = '" & BookID_tb2.Text & "' AND issuedToID = '" & StudentID_tb.Text & "')"
-            Using connection As New MySqlConnection(connectionString)
-                Using command As New MySqlCommand(userQuery, connection)
-                    Try
-                        connection.Open()
-                        Dim reader As MySqlDataReader = command.ExecuteReader()
-                        Dim count As Integer
-                        count = 0
-                        While reader.Read
-                            count = count + 1
-                        End While
-                        If count = 0 Then
-                            MessageBox.Show("This book is not issued to the user.")
-                            Return
-                        End If
-                    Catch ex As Exception
-                        MessageBox.Show("Error: " & ex.Message)
-                    End Try
-                End Using
+            userQuery = "SELECT * FROM lib_borrowed_books WHERE (book_ID = '" & BookID_tb2.Text & "' AND issuedTo = '" & StudentID_tb.Text & "')"
+            'Using connection As New MySqlConnection(connectionString)
+            Using command As New MySqlCommand(userQuery, Con)
+                Try
+                    Con.Open()
+                    Dim reader As MySqlDataReader = command.ExecuteReader()
+                    Dim count As Integer
+                    count = 0
+                    While reader.Read
+                        count = count + 1
+                    End While
+                    If count = 0 Then
+                        MessageBox.Show("This book is not issued to the user.")
+                        Return
+                    End If
+                Catch ex As Exception
+                    MessageBox.Show("Error: " & ex.Message)
+                End Try
+                Con.Close()
             End Using
+            'End Using
 
-            Dim query = "SELECT * FROM borrowed_books WHERE (BookID = '" & BookID_tb2.Text & "')"
-            Using connection As New MySqlConnection(connectionString)
-                Using command As New MySqlCommand(query, connection)
-                    Try
-                        connection.Open()
-                        Dim reader As MySqlDataReader = command.ExecuteReader()
-                        Dim currentDate As DateTime = DateTimePicker1.Value
-                        While reader.Read()
-                            Dim updateQueryInBooks = "UPDATE books SET isIssued = 0, issuedTo = '', dueDate = '0000-00-00 00:00:00' WHERE ID = '" & BookID_tb2.Text & "'"
-                            Dim updateQueryInBorrowed_Books = "DELETE FROM borrowed_books WHERE BookID = '" & BookID_tb2.Text & "'"
-                            Using newConnection As New MySqlConnection(connectionString)
-                                Using newCommand As New MySqlCommand(updateQueryInBooks, newConnection)
-                                    Try
-                                        newConnection.Open()
-                                        newCommand.ExecuteNonQuery()
-                                        If reader("dueDate") < currentDate Then
-                                            Dim fine As Integer = DateDiff(DateInterval.Day, reader("dueDate"), currentDate)
-                                            Dim fineUpdateQuery As String
-                                            If isStudent Then
-                                                fineUpdateQuery = "SELECT * FROM students WHERE ID = '" & StudentID_tb.Text & "'"
-                                            Else
-                                                fineUpdateQuery = "SELECT * FROM faculty WHERE ID = '" & StudentID_tb.Text & "'"
-                                            End If
-                                            Using newNewConnection As New MySqlConnection(connectionString)
-                                                Using newNewCommand As New MySqlCommand(fineUpdateQuery, newNewConnection)
-                                                    Try
-                                                        newNewConnection.Open()
-                                                        Dim newNewReader As MySqlDataReader = newNewCommand.ExecuteReader
-                                                        While newNewReader.Read()
-                                                            Dim value As Integer
-                                                            Integer.TryParse(newNewReader("Fine").ToString, value)
-                                                            fine = fine + value
-                                                        End While
-                                                    Catch ex As Exception
-                                                        MessageBox.Show("Error: " & ex.Message)
-                                                    End Try
-                                                End Using
-                                            End Using
-                                            If isStudent Then
-                                                fineUpdateQuery = "UPDATE students SET Fine = '" & fine & "' WHERE ID = '" & StudentID_tb.Text & "'"
-                                            Else
-                                                fineUpdateQuery = "UPDATE faculty SET Fine = '" & fine & "' WHERE ID = '" & StudentID_tb.Text & "'"
-                                            End If
-                                            Using newNewConnection As New MySqlConnection(connectionString)
-                                                Using newNewCommand As New MySqlCommand(fineUpdateQuery, newNewConnection)
-                                                    Try
-                                                        newNewConnection.Open()
-                                                        newNewCommand.ExecuteNonQuery()
-                                                        'MessageBox.Show("Fine updated to " + fine.ToString)
-                                                    Catch ex As Exception
-                                                        MessageBox.Show("Error: " & ex.Message)
-                                                    End Try
-                                                End Using
-                                            End Using
-                                            Dim addTransactionToAdmin = "INSERT INTO transactions (transaction) VALUES (' " & StudentID_tb.Text & " returned the book with book ID " & addBalance_tb.Text & ", and incurred a fine of " & fine.ToString & "')"
-                                            Using newNewConnection As New MySqlConnection(connectionString)
-                                                Using newNewCommand As New MySqlCommand(addTransactionToAdmin, newNewConnection)
-                                                    Try
-                                                        newNewConnection.Open()
-                                                        newNewCommand.ExecuteNonQuery()
-                                                    Catch ex As Exception
-                                                        MessageBox.Show("Error: " & ex.Message)
-                                                    End Try
-                                                End Using
-                                            End Using
-                                            MessageBox.Show("Since you are returning the book late, some fine has been added to your account. New fine is " + fine.ToString)
-                                        Else
-                                            Dim addTransactionToAdmin = "INSERT INTO transactions (transaction) VALUES (' " & StudentID_tb.Text & " returned the book with book ID " & BookID_tb2.Text & "')"
-                                            Using newNewConnection As New MySqlConnection(connectionString)
-                                                Using newNewCommand As New MySqlCommand(addTransactionToAdmin, newNewConnection)
-                                                    Try
-                                                        newNewConnection.Open()
-                                                        newNewCommand.ExecuteNonQuery()
-                                                    Catch ex As Exception
-                                                        MessageBox.Show("Error: " & ex.Message)
-                                                    End Try
-                                                End Using
-                                            End Using
-                                        End If
-                                        MessageBox.Show("Your book with BookID: " + BookID_tb2.Text.ToString + " has been returned to the library.")
-                                    Catch ex As Exception
-                                        MessageBox.Show("Error: " & ex.Message)
-                                    End Try
-                                End Using
-                            End Using
-                            Using newConnection As New MySqlConnection(connectionString)
-                                Using newCommand As New MySqlCommand(updateQueryInBorrowed_Books, newConnection)
-                                    Try
-                                        newConnection.Open()
-                                        newCommand.ExecuteNonQuery()
-                                    Catch ex As Exception
-                                        MessageBox.Show("Error: " & ex.Message)
-                                    End Try
-                                End Using
-                            End Using
-                        End While
-                    Catch ex As Exception
-                        MessageBox.Show("Error: " & ex.Message)
-                    End Try
-                End Using
+            Dim query = "SELECT * FROM lib_borrowed_books WHERE (book_ID = '" & BookID_tb2.Text & "')"
+            'Using connection As New MySqlConnection(connectionString)
+            Using command As New MySqlCommand(query, Con)
+                Try
+                    'Con.Open()
+                    Dim reader As MySqlDataReader = command.ExecuteReader()
+                    Dim currentDate As DateTime = DateTimePicker4.Value
+                    While reader.Read()
+                        Dim updateQueryInBooks = "UPDATE lib_books SET issued = 0, issuedTo = '', dueDate = '0000-00-00 00:00:00' WHERE book_ID = '" & BookID_tb2.Text & "'"
+                        Dim updateQueryInBorrowed_Books = "DELETE FROM lib_borrowed_books WHERE book_ID = '" & BookID_tb2.Text & "'"
+                        'Using newConnection As New MySqlConnection(connectionString)
+                        Using newCommand As New MySqlCommand(updateQueryInBooks, Con)
+                            Try
+                                Con.Open()
+                                newCommand.ExecuteNonQuery()
+                                If reader("dueDate") < currentDate Then
+                                    Dim fine As Integer = DateDiff(DateInterval.Day, reader("dueDate"), currentDate)
+                                    Dim fineUpdateQuery As String
+                                    'If isStudent Then
+                                    '    fineUpdateQuery = "SELECT * FROM students WHERE ID = '" & StudentID_tb.Text & "'"
+                                    'Else
+                                    '    fineUpdateQuery = "SELECT * FROM faculty WHERE ID = '" & StudentID_tb.Text & "'"
+                                    'End If
+                                    fineUpdateQuery = "SELECT * FROM users WHERE ID = '" & StudentID_tb.Text & "'"
+                                    'Using newNewConnection As New MySqlConnection(connectionString)
+                                    Using newNewCommand As New MySqlCommand(fineUpdateQuery, Con)
+                                        Try
+                                            Con.Open()
+                                            Dim newNewReader As MySqlDataReader = newNewCommand.ExecuteReader
+                                            While newNewReader.Read()
+                                                Dim value As Integer
+                                                Integer.TryParse(newNewReader("Fine").ToString, value)
+                                                fine = fine + value
+                                            End While
+                                        Catch ex As Exception
+                                            MessageBox.Show("Error: " & ex.Message)
+                                        End Try
+                                    End Using
+                                    'End Using
+                                    'If isStudent Then
+                                    '    fineUpdateQuery = "UPDATE students SET Fine = '" & fine & "' WHERE ID = '" & StudentID_tb.Text & "'"
+                                    'Else
+                                    '    fineUpdateQuery = "UPDATE faculty SET Fine = '" & fine & "' WHERE ID = '" & StudentID_tb.Text & "'"
+                                    'End If
+
+
+
+
+
+
+
+                                    ' CHECK   ERROR     ERROR       ERROR
+
+
+                                    fineUpdateQuery = "UPDATE lib_fine SET Fine = '" & fine & "' WHERE ID = '" & StudentID_tb.Text & "'"
+                                    Using newNewConnection As New MySqlConnection(Con)
+                                        Using newNewCommand As New MySqlCommand(fineUpdateQuery, newNewConnection)
+                                            Try
+                                                newNewConnection.Open()
+                                                newNewCommand.ExecuteNonQuery()
+                                                'MessageBox.Show("Fine updated to " + fine.ToString)
+                                            Catch ex As Exception
+                                                MessageBox.Show("Error: " & ex.Message)
+                                            End Try
+                                        End Using
+                                    End Using
+                                    'Dim addTransactionToAdmin = "INSERT INTO transactions (transaction) VALUES (' " & StudentID_tb.Text & " returned the book with book ID " & addBalance_tb.Text & ", and incurred a fine of " & fine.ToString & "')"
+                                    'Using newNewConnection As New MySqlConnection(connectionString)
+                                    '    Using newNewCommand As New MySqlCommand(addTransactionToAdmin, newNewConnection)
+                                    '        Try
+                                    '            newNewConnection.Open()
+                                    '            newNewCommand.ExecuteNonQuery()
+                                    '        Catch ex As Exception
+                                    '            MessageBox.Show("Error: " & ex.Message)
+                                    '        End Try
+                                    '    End Using
+                                    'End Using
+                                    MessageBox.Show("Since you are returning the book late, some fine has been added to your account. New fine is " + fine.ToString)
+                                Else
+                                    'Dim addTransactionToAdmin = "INSERT INTO transactions (transaction) VALUES (' " & StudentID_tb.Text & " returned the book with book ID " & BookID_tb2.Text & "')"
+                                    'Using newNewConnection As New MySqlConnection(connectionString)
+                                    '    Using newNewCommand As New MySqlCommand(addTransactionToAdmin, newNewConnection)
+                                    '        Try
+                                    '            newNewConnection.Open()
+                                    '            newNewCommand.ExecuteNonQuery()
+                                    '        Catch ex As Exception
+                                    '            MessageBox.Show("Error: " & ex.Message)
+                                    '        End Try
+                                    '    End Using
+                                    'End Using
+                                End If
+                                MessageBox.Show("Your book with BookID: " + BookID_tb2.Text.ToString + " has been returned to the library.")
+                            Catch ex As Exception
+                                MessageBox.Show("Error: " & ex.Message)
+                            End Try
+                            Con.Close()
+                        End Using
+                        'End Using
+                        'Using newConnection As New MySqlConnection(connectionString)
+                        Using newCommand As New MySqlCommand(updateQueryInBorrowed_Books, Con)
+                            Try
+                                Con.Open()
+                                newCommand.ExecuteNonQuery()
+                            Catch ex As Exception
+                                MessageBox.Show("Error: " & ex.Message)
+                            End Try
+                            Con.Close()
+                        End Using
+                        'End Using
+                    End While
+                Catch ex As Exception
+                    MessageBox.Show("Error: " & ex.Message)
+                End Try
             End Using
+            'End Using
 
             ' Populate the table with all Books
-            LoadAllBooks()
-            PopulateTable()
+            'LoadAllBooks()
+            'PopulateTable()
 
             ' After returning the book, clear the inputs and show the msg box that it is Returned....
             MsgBox("Book Returned Successfully")
-            LoadAllBooks()
+            'LoadAllBooks()
             BookID_tb2.Text = ""
             addBalance_tb.Text = ""
             StudentID_tb.Text = ""
